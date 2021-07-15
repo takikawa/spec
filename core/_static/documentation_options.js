9dbb095aced4de9c5f79a6ef80313dea520d64c4
takikawa <takikawa@users.noreply.github.com>
2021-07-15 23:25:41 +0000

    deploy: 3ba74adf9c2958e9d1e96d99c443a74afd3e7de6

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.1 (Draft 2021-07-11)',
+    VERSION: '1.1 (Draft 2021-07-15)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
